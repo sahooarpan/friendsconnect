162f5443f891bb2eda4d19adac55ea8cb4e4668c
Arpan Sahoo <arpan.sahoo@ad.infosys.com>
2020-08-22 19:00:45 +0530

    AWS S3 changes made

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
-
+//Middleware
 module.exports = function(req,res,next){
     //Get token from header
     const token = req.header('x-auth-token');
