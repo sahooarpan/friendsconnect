a2807b44f6f3c23e58966e762d1c0d04a3e4913d
Arpan Sahoo <arpan.sahoo@ad.infosys.com>
2020-08-22 19:09:25 +0530

    AWS S3 changes made

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config();
 
-module.exports=keys={
+ module.exports=keys={
     accessKeyId:process.env.accessKeyId || accessKeyId,
     secretAccessKey:process.env.secretAccessKey || secretAccessKey 
 }
